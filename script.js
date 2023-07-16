46bcf24bd5e02a2646f19cd1414b43ec11a9adee
Suzeet88 <119216278+Suzeet88@users.noreply.github.com>
2023-07-16 17:01:37 +0545

    fitbit

---
+++ script.js
@@ -0,0 +1,14 @@
+// make  mobile navigation work
+ 
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click" , function () {
+       headerEl.classList.toggle("nav-open");
+} );
+
+
+
+
+
+
